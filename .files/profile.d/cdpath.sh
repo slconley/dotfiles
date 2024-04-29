db5e69c94f93c387e0598ff0900472c2ca4b4304
sean.conley <slconley@users.noreply.github.com>
2024-04-29 14:22:51 +0000

    makehist path fixes

--- .files/profile.d/cdpath.sh
+++ .files/profile.d/cdpath.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 xcloud=$HOME/xcloud
-CDPATH_BASE=".:$HOME:$HOME/.files:$XDG_RUNTIME_DIR:$HOME/.local:$HOME/.local/cdpath:$HOME/code"
+CDPATH_BASE=".:$HOME:$HOME/.files:$XDG_RUNTIME_DIR:$HOME/.local:$HOME/.local/cdpath:$HOME/.var:$HOME/code"
 CDPATH_BASE="${CDPATH_BASE}:$xcloud:$xcloud/code:$xcloud/code/wip:$xcloud/share:$HOME/share:$HOME/opt/share"
 CDPATH="${CDPATH_BASE}"
 export CDPATH CDPATH_BASE
